ac7ec5b01f5f05056be06f65e703bc2d406aefd0
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-11-07 13:08:42 +0100

    Set up selenium for testing
    Co-Authored-By: ViggoBlomqvist <111963633+Viggoblom@users.noreply.github.com>

---
+++ test/client.test.js
@@ -0,0 +1,34 @@
+import {By, Builder} from 'selenium-webdriver';
+import {suite} from 'selenium-webdriver/testing/index.js';
+import {equal} from 'assert';
+
+suite(function(env) {
+  describe('First script', function() {
+    let driver;
+
+    before(async function() {
+      driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(async () => await driver.quit());
+
+    it('First Selenium script', async function() {
+      await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+
+      const title = await driver.getTitle();
+      equal('Web form', title);
+
+      await driver.manage().setTimeouts({implicit: 500});
+
+      const textBox = await driver.findElement(By.name('my-text'));
+      const submitButton = await driver.findElement(By.css('button'));
+
+      await textBox.sendKeys('Selenium');
+      await submitButton.click();
+
+      const message = await driver.findElement(By.id('message'));
+      const value = await message.getText();
+      equal('Received!', value);
+    });
+  });
+});
