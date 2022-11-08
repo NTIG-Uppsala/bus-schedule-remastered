89c492c4d02cc0c4f15e4a5e181c76ba814d506c
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-11-08 10:07:33 +0100

    Add options when running chrome

--- test/client.test.js
+++ test/client.test.js
@@ -1,12 +1,20 @@
 import {Builder} from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
 import assert from 'assert';
 import 'chromedriver';
 
 describe('First script', function() {
   let driver;
 
+  const chromeOptions = new chrome.Options;
+  chromeOptions.addArguments('--headless');
+  // chromeOptions.addArguments('--no-sandbox');
+  chromeOptions.addArguments('--disable-dev-shm-usage');
+
   before(async function() {
-    driver = await new Builder().forBrowser('chrome').build();
+    driver = await new Builder().forBrowser('chrome')
+        .setChromeOptions(chromeOptions)
+        .build();
   });
 
   after(async () => await driver.quit());
