bec19689650942e6bb5ba7e2de11478fe0716269
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-11-09 15:41:10 +0100

    Add zip files in preparation for testing
    Co-Authored-By: Leo Kempe <leo.max.kempe@gmail.com>

--- DOCUMENTATION/SETUP.md
+++ DOCUMENTATION/SETUP.md
@@ -4,3 +4,6 @@ To set up the project for development:
 - Make sure you have Node.js and npm installed
 - Run `npm install` in your project folder
 - \[Optional\] Install the ESLint extension if you're using VSCode
+- For testing you need a gtfs zip file at ./data/testdata.zip. If the tests stop working you can update it by running ./download_test_data.bat.
+  - To run the file you need a .env file with the following content:
+    - `STATIC_API_KEY="Your API key"`
---
+++ download_test_data.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Downloads data to be used for testing
+:: This script is intended to be run from the root of the repository
+:: You need a .env file in the root of the repository with the following variables:
+:: STATIC_API_KEY="{put your static api key kere}"
+:: Example: STATIC_API_KEY="1234567890"
+
+setlocal
+FOR /F "tokens=*" %%i in ('type .env') DO SET "%%i"
+curl -o ./data/testdata.zip https://opendata.samtrafiken.se/gtfs/ul/ul.zip?key=%STATIC_API_KEY% -H "Accept-Encoding: deflate"
+endlocal
+pause
