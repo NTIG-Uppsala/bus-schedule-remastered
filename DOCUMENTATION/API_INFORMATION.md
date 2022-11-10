ee2c5d53cfb6881354b4b431882a8101c0a0cdaa
Leon-Loov <leon.loov@elev.ga.ntig.se>
2022-11-10 15:01:29 +0100

    Update info about API and server tests

--- DOCUMENTATION/API_INFORMATION.md
+++ DOCUMENTATION/API_INFORMATION.md
@@ -1,3 +1,4 @@
 # API Information
 
 We use [Trafiklab](https://www.trafiklab.se/)'s API, more specifically [GTFS Regional](https://www.trafiklab.se/api/trafiklab-apis/gtfs-regional/) for UL
+We will use the stop_times.txt file to get the next departures from a stop. To link it to bus numbers we can either dynamically cross-reference with the trips.txt and routes.txt files or we can manually tell the program to associate specific `trip_id`s with specific bus numbers. We will use the latter method to begin with.
--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,37 @@
 import * as gtfs from 'gtfs';
+import assert from 'assert';
 
 // Config pointing to our gtfs database for testing
 const config = {
   'agencies': [
     {
       'path': './data/testdata.zip',
-      'exclude': ['shapes'],
+      // All the files we want to KEEP are commented out;
+      // All the files we want to REMOVE are uncommented.
+      // This is a complete list of files our gtfs library looks for.
+      'exclude': [
+        'agency',
+        'areas',
+        'attributions',
+        'calendar_dates',
+        'calendar',
+        'fare_attributes',
+        'fare_leg_rules',
+        'fare_products',
+        'fare_rules',
+        'fare_transfer_rules',
+        'feed_info',
+        'frequencies',
+        'levels',
+        'pathways',
+        // 'routes',
+        'shapes',
+        'stop_areas',
+        // 'stop_times',
+        // 'stops',
+        'transfers',
+        // 'trips',
+      ],
     },
   ],
 };
@@ -18,7 +44,8 @@ describe('GTFS Test', () => {
     it('should read a value from the gtfs database', async () => {
       await gtfs.openDb(config);
       // This stop_id refers to lundellska skolan
-      await gtfs.getStops({stop_id: '9021003700218000'});
+      const stop = await gtfs.getStops({stop_id: '9021003700218000'});
+      assert(stop.length);
     });
   });
 });
