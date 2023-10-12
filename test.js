2ad48106933b057481c416dfa0dde950b4b88916
mohamadhamdan13 <142982637+mohamadhamdan13@users.noreply.github.com>
2023-10-12 09:13:09 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,63 +3,59 @@ import * as url from 'url';
 import * as fs from 'fs';
 
 import express from 'express';
-const app = express();
-const PORT = 8080;
-
 import * as gtfs from 'gtfs';
-
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const app = express();
+const PORT = 8080;
 
 const release = process.env.NODE_ENV === 'production';
 
-// FIX: Handle possible missing files when using fs.readFileSync on config
-// files
-
 // Config for buses and stops
-const config = JSON.parse(fs.readFileSync('./config.json'));
+let config;
+try {
+  config = JSON.parse(fs.readFileSync('./config.json'));
+} catch (err) {
+  console.error('Error reading config file:', err);
+  process.exit(1); // Exit the application on config file error
+}
 
 // Config for GTFS import
 let gtfsConfig;
-if (release || true) {  //FIXA FFS!!!!
-    gtfsConfig = JSON.parse(fs.readFileSync('./gtfs_rel_config.json'));
-    // FIX: Handle missing API key
-    gtfsConfig.agencies[0].url += '?key=' + process.env.STATIC_API_KEY;
+if (release) {
+  gtfsConfig = JSON.parse(fs.readFileSync('./gtfs_rel_config.json'));
+  gtfsConfig.agencies[0].url += '?key=' + process.env.STATIC_API_KEY;
 } else {
-    gtfsConfig = JSON.parse(fs.readFileSync('./gtfs_test_config.json'));
+  gtfsConfig = JSON.parse(fs.readFileSync('./gtfs_test_config.json'));
 }
 
-const maxStopTimes = 100;
 const maxImportTries = 5;
 
-let importSuccess = false;
-
 async function importData() {
-    // TODO: Add gtfsConfig as input param
-    for (let i = 0; i < maxImportTries; i++) {
-        try {
-            // Import GTFS data into the database
-            if (!fs.existsSync(gtfsConfig.sqlitePath)) {
-                await gtfs.importGtfs(gtfsConfig);
-            }
-            await gtfs.openDb(gtfsConfig);
-            importSuccess = true;
-            console.log(`Data imported successfully in ${i + 1} tries`);
-        } catch (err) {
-            console.error(err);
-        }
-        if (importSuccess) {
-            break;
-        }
+  for (let i = 0; i < maxImportTries; i++) {
+    try {
+      if (!fs.existsSync(gtfsConfig.sqlitePath)) {
+        await gtfs.import(gtfsConfig);
+      }
+      await gtfs.openDb(gtfsConfig);
+      console.log(`Data imported successfully in ${i + 1} tries`);
+      return; // Exit the loop if import is successful
+    } catch (err) {
+      console.error(err);
     }
+  }
+  console.error('Failed to import data after multiple attempts');
+  process.exit(1); // Exit the application on import failure
 }
 
 // Call importData to open the database connection
 importData();
 
 app.get('/test/', async (req, res) => {
+<<<<<<< Updated upstream
     const test = gtfs.getTrips({ route_id: "9011003001100000", service_id: 11, direction_id: 0 })
     // console.log(test[0].trip_id)
 
@@ -77,8 +73,19 @@ app.get('/test/', async (req, res) => {
     const stops = gtfs.getStops({
         stop_id: '9022003700021001',
     });
+=======
+  try {
+    const routes = await gtfs.getRoutes();
+    const trips = await gtfs.getTrips(
+    { route_id: "9011003013500000", direction_id: 0 });
+    res.json(trips);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+>>>>>>> Stashed changes
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
