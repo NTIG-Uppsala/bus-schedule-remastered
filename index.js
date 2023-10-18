8285800dceecfae10dcdea174638145d1635fb58
robinofverberg <robin.ofverberg@gmail.com>
2023-10-18 15:41:15 +0200

    Commented code
    Co-Authored-By: Emil Mikkelsen Båge <84081233+EmilMikkelsenB@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ dotenv.config();
 
 // Does nothing ATM.
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-// 
 const release = process.env.NODE_ENV === 'production';
 
 // Config for GTFS import
@@ -28,7 +27,7 @@ if (release == true) {
 
 
 async function importData() {
-    // TODO: Add gtfsConfig as input param
+    // Tries to import, if it fails it will try again until it succeeds or maxImportTries is reached
     for (let i = 0; i < maxImportTries; i++) {
         try {
             // Import GTFS data into the database
@@ -46,9 +45,10 @@ async function importData() {
         }
     }
 }
-// Call importData to open the database connection
 importData();
+
 app.get('/NTIBusScreen/', async (req, res) => {
+    // Accesses the google sheet for admins to add stops and headsigns
     const auth = new google.auth.GoogleAuth({
         keyFile: "credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -63,56 +63,55 @@ app.get('/NTIBusScreen/', async (req, res) => {
     });
     const sheetInput = getRows.data["values"];
 
-    let stopidList = [];
+    let stopIdList = [];
     let headsignList = [];
 
     async function getInfoFromSheet() {
         const result = [];
-
+        // Loops for each cell in google sheet
         for (let i = 0; i < sheetInput.length; i++) {
             const sheetStopName = sheetInput[i][0];
             let direction = sheetInput[i][1];
             const headsign = sheetInput[i][2];
-
-            const getAllstops = gtfs.getStops();
+            const getAllstops = gtfs.getStops(); // Gets all stops from GTFS
+            // Searches for the stop name and direction in GTFS
             const foundStop = getAllstops.find(item => item.stop_name === sheetStopName && item.platform_code === direction);
-
+            // If the stop is found, it will add the stop_id to a list
             if (foundStop) {
-                stopidList.push(foundStop.stop_id);
+                stopIdList.push(foundStop.stop_id);
                 headsignList.push(headsign);
                 sheetInput.push(sheetStopName);
 
             }
         }
-
-        for (let i = 0; i < stopidList.length; i++) {
-            const stopId = stopidList[i];
+        // Loops for each stop in the list and pushes it to the result array
+        for (let i = 0; i < stopIdList.length; i++) {
+            const stopId = stopIdList[i];
             const headsign = headsignList[i];
             const stopName = sheetInput[i][0];
             const respone = await getStoptimesWithHeadsign(stopId, headsign);
             result.push({ stopId, stopName, headsign, ...respone });
         }
-
+        //prints the result array to the screen
         res.json(result);
     }
-
     getInfoFromSheet();
 
+    // Gets the next arrival time for a specific stop and headsign
     async function getStoptimesWithHeadsign(stopId, headsign) {
         const getBuss = gtfs.getStoptimes({
             stop_id: stopId,
             stop_headsign: headsign
         });
         let allBusses = [];
-
+        // Loops for each bus and pushes the arrival time to a list and sorts it to the nearest arrival time
         const bussSorting = getBuss.map(item => ({ arrivalTime: item.arrival_time }));
         allBusses.push(...bussSorting);
-
         allBusses.sort((a, b) => a.arrivalTime.localeCompare(b.arrivalTime));
-
+        // Gets current time and finds the next arrival time
         const currentTime = moment();
         const nextArrival = allBusses.find(item => moment(item.arrivalTime, 'HH:mm:ss').isAfter(currentTime));
-
+        // Returns the next arrival time
         if (nextArrival) {
             return { nextArrivalTime: nextArrival.arrivalTime };
         } else {
