a2387e2d41eb21bb200b44df360f1bc50552a997
SamuelWidlund <142985254+SamuelWidlund@users.noreply.github.com>
2023-11-09 10:15:56 +0100

    first push of test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,39 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Add this to your Cypress commands (in commands.js or support/index.js)
+
+
+import { google } from "googleapis";
+
+Cypress.Commands.add("getGoogleSheetData", async () => {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: 'credentials.json',
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    return auth.getClient()
+      .then((client) => {
+        const googleSheets = google.sheets({ version: "v4", auth: client });
+        const spreadsheetId = "1XW0cmrudu_FTS7BwioJpQsrJeMvYy6J3tYoabZkbcKY";
+        return googleSheets.spreadsheets.values.get({
+          auth,
+          spreadsheetId,
+          range: "sheet1!B2:D100",
+        });
+      })
+      .then((getRows) => getRows.data["values"]);
+   });
+   // Use the custom command in your test
+   describe('Tests', () => {
+    it('correctBusShown', () => {
+      cy.visit('127.0.0.1:8080/NTIBusScreen');
+      cy.getGoogleSheetData().then((sheetInput) => {
+        for (let i = 0; i < sheetInput.length; i++) {
+          cy.log('tjena');
+          cy.log(sheetInput[i][2]); 
+          cy.contains(sheetInput[i][2]);
+        }
+      });
+    });
+   });
