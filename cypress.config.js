ff686500ab66f0e0a11587b3a22b82f3388bb93f
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-11-09 15:26:02 +0100

    add automatic download of sheet to json

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from 'cypress';
-import * as XLSX from 'xlsx/xlsx.mjs';
-import * as fs from 'fs';
-import * as path from 'path';
-
-XLSX.set_fs(fs);
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('task', {
-        convertXlsxToJson( filePath ) {
-          const workbook = XLSX.readFile(filePath);
-          const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-          const data = XLSX.utils.sheet_to_json(worksheet);
-          
-          const fileName = path.basename(filePath, '.xlsx');
-          const jsonFilePath = `./cypress/fixtures/${fileName}.json`;
-          fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2));
-          return null;
-        },
-      })
     },
     baseUrl: 'http://localhost:8080/NtiBusScreen/'
   },
---
+++ download_sheet.js
@@ -0,0 +1,48 @@
+import { google } from 'googleapis';
+import fs from 'fs';
+import * as XLSX from 'xlsx/xlsx.mjs';
+import * as path from 'path';
+import { promise } from 'selenium-webdriver';
+XLSX.set_fs(fs);
+
+async function downloadFile() {
+    return new Promise((resolve, reject) => {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: 'credentials.json',
+            scopes: 'https://www.googleapis.com/auth/drive',
+        });
+
+        const drive = google.drive({ version: 'v3', auth: auth });
+
+        const fileId = '1XW0cmrudu_FTS7BwioJpQsrJeMvYy6J3tYoabZkbcKY';
+        const dest = fs.createWriteStream('data/busdata.xlsx');
+
+        drive.files.export({ fileId, mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }, { responseType: 'stream' })
+            .then(res => {
+                res.data
+                    .on('end', () => {
+                        console.log('Done downloading file.');
+                    })
+                    .on('error', err => {
+                        console.error('Error downloading file.');
+                        reject(err);
+                    })
+                    .pipe(dest)
+                    .on('finish', () => {
+                        resolve();
+                    });
+            });
+    });
+}
+async function main() {
+    await downloadFile();
+
+    const workbook = XLSX.readFile("data/busdata.xlsx");
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = XLSX.utils.sheet_to_json(worksheet);
+    const fileName = path.basename("busdata", '.xlsx');
+    const jsonFilePath = `./cypress/fixtures/${fileName}.json`;
+    fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2));
+}
+
+main().catch(console.error);
