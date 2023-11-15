93c9bdb0d5fb1defa3bfb9e3e74139fe0ebff6d8
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-11-15 13:47:06 +0100

    add test for right next bus time
    Co-Authored-By: Carl Lagerstedt <105862361+Carl595@users.noreply.github.com>
    Co-Authored-By: robinofverberg <91734293+robinofverberg@users.noreply.github.com>
    Co-Authored-By: SamuelWidlund <142985254+SamuelWidlund@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,7 +10,6 @@ describe('Tests', () => {
         cy.get('@busData').then((busData) => {
             for (const bus of busData) {
                 if (bus.Bussnamn != undefined) {
-                    console.log(bus.Bussnamn)
                     cy.contains(bus.Bussnamn);
                 }
             }
@@ -20,5 +19,11 @@ describe('Tests', () => {
     it('nextBusListNotEmpty', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-08T23:59:59');
         cy.contains('"upcomingBusses":[]').should('not.exist');
+    });
+
+    it('nextBusIsRight', () => {
+        cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-15T09:00:00');
+        cy.contains('Ärna","upcomingBusses":[{"arrivalTime":"09:08:00"},{"arrivalTime":"09:21:00"}]}')
+        cy.contains('Fyrislund","upcomingBusses":[{"arrivalTime":"09:10:00"},{"arrivalTime":"09:25:00"}]}')
     });
 });
