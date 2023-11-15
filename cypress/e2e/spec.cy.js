1204900dcc98d95e7ac91aa09bb434f1dd5885d2
Simon Clavensjö <simon.clavensjo@elev.ntig.se>
2023-11-15 15:13:46 +0100

    fix grammar in tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,12 +18,12 @@ describe('Tests', () => {
 
     it('nextBusListNotEmpty', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-08T23:59:59');
-        cy.contains('"upcomingBusses":[]').should('not.exist');
+        cy.contains('"upcomingBuses":[]').should('not.exist');
     });
 
     it('nextBusIsRight', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-15T09:00:00');
-        cy.contains('Ärna","upcomingBusses":[{"arrivalTime":"09:08:00"},{"arrivalTime":"09:21:00"}]}')
-        cy.contains('Fyrislund","upcomingBusses":[{"arrivalTime":"09:10:00"},{"arrivalTime":"09:25:00"}]}')
+        cy.contains('Ärna","upcomingBuses":[{"arrivalTime":"09:08:00"},{"arrivalTime":"09:21:00"}]}')
+        cy.contains('Fyrislund","upcomingBuses":[{"arrivalTime":"09:00:02"},{"arrivalTime":"09:10:02"}]}')
     });
 });
