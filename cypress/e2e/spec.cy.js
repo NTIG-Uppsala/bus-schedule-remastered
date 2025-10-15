debb70a0246af33f0cfdd0b9e5090a278a040745
willebergsten <willebergsten@outlook.com>
2025-10-15 10:47:19 +0200

    gitignore
    made it so that busdata.json and busdata.xlsx are gitignored

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,20 +18,20 @@ describe('Tests', () => {
 
     it('nextBusIsRight', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-15T09:00:00');
-        cy.contains('8 min')
-        cy.contains('Nästa 09:21')
+        cy.contains('7 min')
+        cy.contains('Nästa 09:16')
         cy.contains('nu')
         cy.contains('Nästa 09:10')
     });
 
     it('testRealTimeData', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-17T11:28:00');
-        cy.contains('Nästa 11:54')
+        cy.contains('Nästa 11:50')
     });
 
     it('testCancelledBus', () => {
         cy.visit('127.0.0.1:8080/NTIBusScreen/2023-11-17T11:28:00');
-        cy.contains('14 min')
-        cy.contains('Nästa 12:11')
+        cy.contains('15 min')
+        cy.contains('Nästa 12:03')
     });
 });
